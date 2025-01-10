2df09e136b3908eb31889de7775100b2e52a7036
Blake Torrey <btorrey1998@gmail.com>
2025-01-09 20:21:35 -0700

    Created the first parts of the user model, will need to look up the email validation methods available.

---
+++ src/models/User.ts
@@ -0,0 +1,46 @@
+import { Schema, Document, model, ObjectId } from 'mongoose';
+
+interface IUser extends Document {
+    username: String;
+    email: String;
+    thoughts: ObjectId[];
+    friends: ObjectId[];
+}
+
+const userSchema = new Schema<IUser>(
+    {
+        username: String,
+        email: String, // look up validation methods for emails using mongoose
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+
+
+
+const User = model('user', userSchema);
+
+export default User;
+
+
+// username (string, unique, required, trimmed)
+// email (string required unique must match validation)
+// throughts (array of _id values referencing the thought model)
+// friends (array of _id values referencing the user model)
